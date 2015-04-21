b0f70db74762ed5b5d2175e1d391d16a0409685a
lhamello <lhamello@gmail.com>
2015-04-20 23:16:12 -0300

    atualizando

--- rest-ws/src/main/java/br/unisc/pos/produto/ProdutoRest.java
+++ rest-ws/src/main/java/br/unisc/pos/produto/ProdutoRest.java
@@ -9,9 +9,16 @@ import javax.ejb.TransactionAttributeType;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.CacheControl;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.EntityTag;
 import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import br.unisc.pos.infra.Erro;
 
 @Path("/")
 @Produces("application/json;charset=utf-8")
@@ -23,7 +30,40 @@ public class ProdutoRest implements Serializable {
 
     @Inject
     private ProdutoService produtoService;
+    
+    @GET
+    @Path("produto/{id:[0-9][0-9]*}")
+    public Response buscarPorId(@PathParam("id") Long id, @Context Request request) {
+        Produto produto;
 
+        produto = produtoService.consultarChave(id);
+        
+        if (produto == null) {
+            Erro erro = new Erro(Status.NOT_FOUND, "Id [" + id + "] não encontrado.");
+            
+            return Response.status(Status.NOT_FOUND).entity(erro).build();
+        }
+
+        CacheControl cacheControl = new CacheControl();
+        cacheControl.setMaxAge(100);
+
+        EntityTag tag = new EntityTag(Integer.toString(produto.hashCode()));
+
+        Response.ResponseBuilder builder = request.evaluatePreconditions(tag);
+
+        if (builder != null) {
+            builder.cacheControl(cacheControl);
+
+            return builder.build();
+        }
+
+        builder = Response.ok(produto);
+        builder.cacheControl(cacheControl);
+        builder.tag(tag);
+
+        return builder.build();
+    } 
+    
     @GET
     @Path("produtos")
     public List<Produto> listarTodos(@Context Request request) {
--- rest-ws/src/main/java/br/unisc/pos/produto/perfume/PerfumeRest.java
+++ rest-ws/src/main/java/br/unisc/pos/produto/perfume/PerfumeRest.java
@@ -10,17 +10,11 @@ import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.CacheControl;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.EntityTag;
 import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.UriBuilder;
-
-import br.unisc.pos.infra.Erro;
 
 @Path("/")
 @Produces("application/json;charset=utf-8")
@@ -33,38 +27,6 @@ public class PerfumeRest implements Serializable {
     @Inject
     private PerfumeService perfumeService;
 
-    @GET
-    @Path("perfume/{id:[0-9][0-9]*}")
-    public Response buscarPorId(@PathParam("id") Long id, @Context Request request) {
-        Perfume perfume;
-
-        perfume = perfumeService.consultarChave(id);
-        
-        if (perfume == null) {
-            Erro erro = new Erro(Status.NOT_FOUND, "Id [" + id + "] não encontrado.");
-            
-            return Response.status(Status.NOT_FOUND).entity(erro).build();
-        }
-
-        CacheControl cacheControl = new CacheControl();
-        cacheControl.setMaxAge(100);
-
-        EntityTag tag = new EntityTag(Integer.toString(perfume.hashCode()));
-
-        Response.ResponseBuilder builder = request.evaluatePreconditions(tag);
-
-        if (builder != null) {
-            builder.cacheControl(cacheControl);
-
-            return builder.build();
-        }
-
-        builder = Response.ok(perfume);
-        builder.cacheControl(cacheControl);
-        builder.tag(tag);
-
-        return builder.build();
-    }
     
     @POST
     public Response inserir(Perfume perfume) {
